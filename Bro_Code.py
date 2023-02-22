c4435489551c5d4eacb9474dc6bbc6dc8d909b41
StonesRocks <68532873+StonesRocks@users.noreply.github.com>
2023-02-22 17:52:08 +0100

    added matrix array and keyboard class

--- Bro_Code.py
+++ Bro_Code.py
@@ -41,16 +41,49 @@ with open(json_file, encoding="utf8") as f:
                     elif key == 'a' or key == 'f' or key == 'f2' or key == 'p' or key == 's':
                         "something about font"
         skipy += 1
+    #puts every kex and key into a numpy array where each index represents a key.
     ken_array = np.array(list(ken_list))
 
-matrix_size = len(ken_array)
+#looks up the smallest matrix available, if matrix is square then columns and row will match, otherwise add extra column
+ken_size = len(ken_array)
+man_size = 0 #value given after
 max = 0
 may = 0
-if np.sqrt(matrix_size) == int(np.sqrt(matrix_size)):
-    max = np.sqrt(matrix_size)
+if np.sqrt(ken_size) == int(np.sqrt(ken_size)):
+    max = np.sqrt(ken_size)
     may = max
 else:
-    max = int(np.sqrt(matrix_size)) + 1
-    may = int(np.sqrt(matrix_size))
+    max = int(np.sqrt(ken_size)) + 1
+    may = int(np.sqrt(ken_size))
+man_size = max * may
+print(man_size)
+#create an array with all rows and columns.
+man_array = np.empty
+rows = []
+k = 0
+for i in range(may):
+    onerow = []
+    onerow.clear()
+    for j in range(0,max):
+        onerow.append(k)
+        k += 1
+    rows.append(onerow)
+man_array = np.array(list(rows))
 
-print(ken_array)
+#should i create a class for the individual in population?
+#the class would include the list of ken2man and fitness.
+
+#ken2man or man2ken?
+#we need man2ken to be able to calculat the length/fitness
+#we need ken2man to be able know which man exist in keyboard.
+    #why cant we just have a list of the existing man and then use each index as the ken?
+    #we look up which index the man have and take all the index to calculate kex and key?
+
+class Keyboard:
+    def __init__(self):
+        self.fitness = 0
+        self.man = np.random.choice(man_size, size=ken_size, replace=False)
+
+
+
+
